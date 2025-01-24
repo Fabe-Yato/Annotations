e125af12f00ca95ab42e5665d43a2aed8cfab81f
Fabiano <fabibinho2@gmail.com>
2025-01-24 00:28:39 -0300

    Instalando a dependência do cors e configurando no projeto

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import "./config/dbConfig.js";
 import routes from "./routes.js"
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(routes)
+app.use(cors());
 
 app.listen(3000, () => {
     console.log("O servidor está rodando na porta: http://localhost:3000 ");
