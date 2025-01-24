daa38a4b9ba659cba20dc04cca3cb928e9921190
Fabiano <fabibinho2@gmail.com>
2025-01-24 02:24:51 -0300

    Parte do frontend feita, adicionei as partes principais do design

---
+++ frontend/src/App.jsx
@@ -0,0 +1,40 @@
+import "./app.css"
+import "./global.css"
+import "./sidebar.css"
+import "./main.css"
+import Notes from "./components/Notes/Notes"
+
+function App() {
+  return (
+    <>
+      <div id="app">
+        <aside>
+          <h2>Caderno de Notas</h2>
+          <form>
+            
+            <div className="input-block">
+              <label htmlFor="title">Título da Anotação</label>
+              <input/>
+            </div>
+            
+            <div className="input-block">
+              <label htmlFor="nota">Anotação</label>
+              <textarea></textarea>
+            </div>
+
+            <button type="submit">Salvar</button>
+          </form>
+        
+        </aside>
+        <main>
+            <ul>
+              <Notes />
+            </ul>
+        </main>
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Notes/Notes.jsx
@@ -0,0 +1,19 @@
+const Notes = () => {
+    return(
+        <>
+            <li className="notepad-infos">
+                <div>
+                    <h2>Fazer Compras</h2>
+                    <div>
+                        X
+                    </div>
+                </div>
+
+                <textarea>lorem ipsum, lorem lorem ipsum ipmsum lorisum</textarea>
+                <span>!</span>
+            </li>
+        </>
+    )
+}
+
+export default Notes;
